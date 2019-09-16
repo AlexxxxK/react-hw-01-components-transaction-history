dc42a7673053c131722b1c63a135822020287f4f
AlexxxxK <olekdansr.kozyr@outlook.com>
2019-09-16 07:24:35 +0300

    transactiona history

---
+++ build/precache-manifest.b6475f08446b0128faa6e547d672d12e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b20d0644f5edd01b56bae7401a855c8b",
+    "url": "/react-hw-01-components-transaction-history/index.html"
+  },
+  {
+    "revision": "f0927e6348080c9e9857",
+    "url": "/react-hw-01-components-transaction-history/static/css/main.9d5f5623.chunk.css"
+  },
+  {
+    "revision": "947e545b93495910f887",
+    "url": "/react-hw-01-components-transaction-history/static/js/2.cb4678ed.chunk.js"
+  },
+  {
+    "revision": "f0927e6348080c9e9857",
+    "url": "/react-hw-01-components-transaction-history/static/js/main.efec18c1.chunk.js"
+  },
+  {
+    "revision": "27e704ee92009bfd0e76",
+    "url": "/react-hw-01-components-transaction-history/static/js/runtime~main.c2c18fcc.js"
+  }
+]);
