4f546fcdeb34e9cd3fc85b7b1e78c023548e447d
AlexxxxK <olekdansr.kozyr@outlook.com>
2019-09-16 08:59:49 +0300

    Updates

---
+++ precache-manifest.3c598ca04d57da397d1cdd76dc083c4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867e7b948aa0978da94d59a7309e004f",
+    "url": "/react-hw-01-components-transaction-history/index.html"
+  },
+  {
+    "revision": "ee885b6e47c3bf71bc2f",
+    "url": "/react-hw-01-components-transaction-history/static/css/main.5f790e67.chunk.css"
+  },
+  {
+    "revision": "947e545b93495910f887",
+    "url": "/react-hw-01-components-transaction-history/static/js/2.cb4678ed.chunk.js"
+  },
+  {
+    "revision": "ee885b6e47c3bf71bc2f",
+    "url": "/react-hw-01-components-transaction-history/static/js/main.efec18c1.chunk.js"
+  },
+  {
+    "revision": "27e704ee92009bfd0e76",
+    "url": "/react-hw-01-components-transaction-history/static/js/runtime~main.c2c18fcc.js"
+  }
+]);
